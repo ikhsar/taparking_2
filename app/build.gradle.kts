eb0018b74b3b61b43d9bf5b50d7e1eb847550438
Dimas Putra <dimaspm008@gmail.com>
2024-08-15 19:49:17 +0700

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.5.0"
     id("org.jetbrains.kotlin.android") version "1.8.0"
-    id ("com.google.gms.google-services")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -29,7 +29,18 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            force("com.google.firebase:firebase-auth:23.0.0")
+            force("com.google.firebase:firebase-database:20.3.0")
+            force("com.google.firebase:firebase-crashlytics:18.6.0")
         }
     }
 }
@@ -39,14 +50,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.activity:activity:1.8.0")
-    implementation ("com.google.firebase:firebase-auth:22.1.2")
-    implementation ("com.google.firebase:firebase-database:20.2.0")
-    implementation(libs.firebase.crashlytics.buildtools)
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-crashlytics:18.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-
